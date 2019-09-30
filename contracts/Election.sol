f6ea1b2a49bda532523266971bf3c674453c7a5c
dalibouomrane <dalibouomrane@gmail.com>
2019-09-30 21:56:51 +0100

    initial commit of Voteum

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+
+contract Election {
+//store candidate
+//Read candidate
+string public candidate;
+//constructor
+    constructor() public {
+        candidate = "Kais saayed";
+
+    }
+
+}
